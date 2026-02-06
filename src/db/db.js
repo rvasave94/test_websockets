e3c03beda49da4aef81dfab13e706418e7b033f3
RajV <rvasave94@gmail.com>
2026-02-06 23:48:46 +0530

    implement database schemas

---
+++ src/db/db.js
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/neon-serverless";
+import { Pool, neonConfig } from "@neondatabase/serverless";
+import ws from "ws";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not defined");
+}
+
+neonConfig.webSocketConstructor = ws;
+
+export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+export const db = drizzle(pool);
